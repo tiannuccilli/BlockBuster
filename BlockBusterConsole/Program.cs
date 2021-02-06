4b613efae347c23a3a9e4c037d65ed5a63d81c02
Talia Iannuccilli <tsiannuccilli@email.neit.edu>
2021-02-06 08:49:32 -0500

    wk4 work

---
+++ BlockBusterConsole/Program.cs
@@ -0,0 +1,85 @@
+﻿using BlockBuster;
+using BlockBuster.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace BlockBusterConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var conOhType = args[0].ToLower();
+            var ohType = "";
+            var askType = true;
+            while (askType == true)
+            {
+                Console.WriteLine("How would you like to recieve movies?(console/csv)");
+                var ohTypetemp = Console.ReadLine();
+                ohType = ohTypetemp.ToLower();
+                if (ohType == "console" || ohType == "csv")
+                {
+                    askType = false;
+                }
+                Console.Clear();
+            }
+            
+            var findBy = "";
+            var movie = new Movie();
+            List<Movie> movies = new List<Movie>();
+            var ask = true;
+            while (ask == true)
+            {
+                Console.WriteLine("How would you like to search movies?(all, id, genre, director)");
+                var findByTemp = Console.ReadLine();
+                findBy = findByTemp.ToLower();
+                if (findBy == "all")
+                {
+                    movies = BlockBusterBasicFunctions.GetAllMovies();
+                    ask = false;
+                }
+                else if (findBy == "id")
+                {
+                    movie = BlockBusterBasicFunctions.GetMovieById(5);
+                    ask = false;
+                }
+                else if (findBy == "genre")
+                {
+                    movies = BlockBusterBasicFunctions.GetAllGenreMovies("action");
+                    ask = false;
+                }
+                else if (findBy == "director")
+                {
+                    movies = BlockBusterBasicFunctions.GetAllDirectorMovies("Spielberg");
+                    ask = false;
+                }
+                else
+                {
+                    Console.WriteLine("Not a proper search case (all, id, genre, director)");
+                }
+            }
+
+            var oh = new outputHelper();
+            if(findBy == "id")
+            {
+                if (ohType == "console")
+                {
+                    oh.writeToConsole1(movie);
+                }
+                else if (ohType == "csv")
+                {
+                    oh.writeToCSV1(movie);
+                }
+            }
+            else if (ohType == "console")
+            {
+                oh.writeToConsole(movies);
+            }
+            else if (ohType == "csv"){
+                oh.writeToCSV(movies);
+            }
+        }
+    }
+}
---
+++ BlockBusterConsole/outputHelper.cs
@@ -0,0 +1,46 @@
+﻿using BlockBuster.Models;
+using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
+
+namespace BlockBusterConsole
+{
+    class outputHelper
+    {
+        public void writeToConsole(List<Movie> movies)
+        {
+            Console.WriteLine("List of Movies");
+            foreach (var m in movies)
+            {
+                Console.WriteLine($"MovieID: {m.MovieId}    Title: {m.Title}    Release Year:{m.ReleaseYear}");
+
+            }
+        }
+        public void writeToConsole1(Movie movie)
+        {
+            Console.WriteLine("Movie");
+
+                Console.WriteLine($"MovieID: {movie.MovieId}    Title: {movie.Title}    Release Year:{movie.ReleaseYear}");
+        }
+
+        public void writeToCSV(List<Movie> movies)
+        {
+            using (var writer = new StreamWriter(@"..\file.csv"))
+            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            {
+                csv.WriteRecords(movies);
+            }
+        }
+        public void writeToCSV1(Movie movie)
+        {
+            using (var writer = new StreamWriter(@"..\file.csv"))
+            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            {
+                csv.WriteRecord(movie);
+            }
+        }
+    }
+}
